71be8ef6d074eefebfb47f356a03f8038af73f97
Nikita D <n.damnation@gmail.com>
2023-04-13 10:30:54 +0300

    Добавлена настройка "Начало семестра"

--- WinFormsAppProject/Form1.Designer.cs
+++ WinFormsAppProject/Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dateTimePicker1 = new DateTimePicker();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(296, 158);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(339, 201);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Сохранить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(187, 164);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Начало семестра:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(dateTimePicker1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private Button button1;
+        private Label label1;
     }
 }
--- WinFormsAppProject/Form1.cs
+++ WinFormsAppProject/Form1.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 namespace WinFormsAppProject
 {
     public partial class Form1 : Form
@@ -5,6 +7,24 @@ namespace WinFormsAppProject
         public Form1()
         {
             InitializeComponent();
+            Settings.Init(ref dateTimePicker1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, Control> settigsElements = GetSettingElements();
+            foreach (var element in settigsElements)
+            {
+                long StampStart = Settings.ConvertToTimestamp(dateTimePicker1.Text);
+                Settings.Save(element.Key, StampStart);
+            }
+        }
+
+        private Dictionary<string, Control> GetSettingElements()
+        {
+            return new Dictionary<string, Control> {
+                { "SemestrBeginning", dateTimePicker1 }
+            };
         }
     }
 }
