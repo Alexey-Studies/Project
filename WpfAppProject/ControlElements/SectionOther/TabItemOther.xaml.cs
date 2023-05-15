78bc34c24104c86b5de38299fe7bcc2cae24e049
Darya B <beletskaya.daryaa@mail.ru>
2023-05-15 23:28:21 +0300

    Сделана адаптивная вёрстка раздела “Прочее”

---
+++ WpfAppProject/ControlElements/SectionOther/TabItemOther.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppProject.ControlElements.SectionOther
+{
+    /// <summary>
+    /// Логика взаимодействия для TabItemOther.xaml
+    /// </summary>
+    public partial class TabItemOther : UserControl
+    {
+        public TabItemOther()
+        {
+            InitializeComponent();
+        }
+        private void Button_ClickImport(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Document";
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = "Text documents (.txt)|*.txt";
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dialog.FileName;
+            }
+        }
+
+        private void Button_ClickExport(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            var dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.FileName = "Document";
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = "Text documents (.txt)|*.txt";
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dialog.FileName;
+            }
+        }
+    }
+
+}
