84dc4d9b69097d5e23b9609104bc9ca5ceaa5c03
Darya B <beletskaya.daryaa@mail.ru>
2023-05-18 17:24:23 +0300

    Сделан дизайн переключателей вкладок
    Co-Authored-By: Alexey V. <130074040+Alexey-Studies@users.noreply.github.com>
    Co-Authored-By: Catrep <100048309+Catrep@users.noreply.github.com>

---
+++ WpfAppProject/Converters/TabSizeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfAppProject.Converters
+{
+    internal class TabSizeConverter : IMultiValueConverter
+    {
+        public object Convert(object[] tabs, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            TabControl tabControl = tabs[0] as TabControl;
+            double width = tabControl.ActualWidth / tabControl.Items.Count;
+
+            return (width <= 1) ? 0 : (width - 1);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
